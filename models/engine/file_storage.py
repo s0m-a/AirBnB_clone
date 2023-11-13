5e1922305eaa02b0d4a204071ef44ed2dd71b969
ogbenna nmesoma anita <ogbennaanita@gmail.com>
2023-11-12 20:38:21 -0800

    updates

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -26,6 +26,7 @@ class FileStorage:
         with open(FileStorage.__file_path, "w", encoding="utf-8") as f:
             d = {k: v.to_dict() for k, v in FileStorage.__object.items()}
             json.dump(d, f)
+
     def clses(self):
         """Returns a dict of valid classes and reference"""
         from models.base_model import BaseModel
@@ -37,12 +38,12 @@ class FileStorage:
         from models.review import Review
 
         clses = {"BaseModel": BaseModel,
-                   "User": User,
-                   "State": State,
-                   "City": City,
-                   "Amenity": Amenity,
-                   "Place": Place,
-                   "Review": Review}
+                 "User": User,
+                 "State": State,
+                 "City": City,
+                 "Amenity": Amenity,
+                 "Place": Place,
+                 "Review": Review}
         return clses
 
     def reload(self):
@@ -55,4 +56,40 @@ class FileStorage:
             obj_d = json.load(f)
             obj_d = {k: self.clses()[v["__class__"]](**v)
                      for k, v in obj_d.items()}
-  
+
+    def attributes(self):
+        """Returns attributes and types for className"""
+        attributes = {
+            "BaseModel":
+                     {"id": str,
+                      "created_at": datetime.datetime,
+                      "updated_at": datetime.datetime},
+            "User":
+                     {"email": str,
+                      "password": str,
+                      "first_name": str,
+                      "last_name": str},
+            "State":
+                     {"name": str},
+            "City":
+                     {"state_id": str,
+                      "name": str},
+            "Amenity":
+                     {"name": str},
+            "Place":
+                     {"city_id": str,
+                      "user_id": str,
+                      "name": str,
+                      "description": str,
+                      "number_rooms": int,
+                      "number_bathrooms": int,
+                      "max_guest": int,
+                      "price_by_night": int,
+                      "latitude": float,
+                      "longitude": float,
+                      "amenity_ids": list},
+            "Review":
+            {"place_id": str,
+                         "user_id": str,
+                         "text": str}
+        }
